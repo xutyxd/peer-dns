085c73ca935d196cf78d03a2e56736fa91278bde
Xuty <xutyxd@gmail.com>
2023-11-01 21:25:37 +0100

    feat(dependency): Remove rxjs, was unused

--- src/classes/peer.class.ts
+++ src/classes/peer.class.ts
@@ -1,21 +1,14 @@
 
-import { Subject } from "rxjs";
 import { Socket } from "socket.io";
 
 export class Peer {
 
-    public on = {
-        room: new Subject<{room: string, socket: Socket}>()
-    }
-
     public readonly id: string;
-    private room?: string;
 
-    constructor(private socket: Socket) {   
+    constructor(socket: Socket) {   
         this.id = socket.id;
 
         socket.on('query', (room) => {
-            console.log('On query: ', room);
             socket.join(room);
             socket.to(room).emit('link', { id: socket.id });
         });
@@ -27,30 +20,5 @@ export class Peer {
                 socket.to(data.id).emit(event, { ...data, id: this.id })
             });
         });
-    }
-
-    public connect(offer?: string) {
-        if (!this.room) {
-            return;
-        }
-
-        this.socket.to(this.room).emit('link', { offer, id: this.id });
-    }
-
-    public candidates = {
-        export: () => {
-            if (!this.room) {
-                return;
-            }
-
-            this.socket.to(this.room).emit('candidatesExport', { id: this.id });
-        },
-        import: (candidates: string) => {
-            if (!this.room) {
-                return;
-            }
-
-            this.socket.to(this.room).emit('candidatesImport', { candidates, id: this.id });
-        }
     }
 }
