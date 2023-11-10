8aae96d9abb31018b71761bcea8c9abb3e633432
Xuty <xutyxd@gmail.com>
2023-11-10 16:09:32 +0100

    feat(exit): Added event to allow peers to exit from a room

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-# Peer DNS
+# Peer DNS ![NPM Downloads](https://img.shields.io/npm/dw/peer-dns)
 Another socket server for signaling peers
 
 
 ## Description
-Server that listen 3 events on sockets.
+Server that listen 4 events on sockets.
 - Query: Event to join peer in a room
 - Link: Event to request offers and answer them
 - Candidates: Event to export/import candidates
+- Exit: To leave from a room
 
 ## Installation
 
--- src/classes/peer.class.ts
+++ src/classes/peer.class.ts
@@ -13,10 +13,15 @@ export class Peer {
             socket.to(room).emit('link', { id: socket.id });
         });
 
+        socket.on('exit', (room) => {
+            socket.leave(room);
+        });
+
         const events = ['link', 'candidates'];
 
         events.forEach((event) => {
             socket.on(event, (data) => {
+                console.log('Emitting data: ', data);
                 socket.to(data.id).emit(event, { ...data, id: this.id })
             });
         });
