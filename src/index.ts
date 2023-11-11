51087a13d200106d4c9e4d8dd3bfe1f51f3c2120
Xuty <xutyxd@gmail.com>
2023-11-11 17:51:04 +0100

    feat(alive): Try to mantain server always alive with fetch every 4 minutes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 
+import { createServer } from 'node:http';
 import { SocketServer } from './classes/socket-server.class';
 
 
 class Server {
     public server;
     constructor() {
-        this.server = new SocketServer();
+
+        const server = createServer((request, response) => {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(`I'm alive`, 'utf-8');
+        });
+
+        this.server = new SocketServer(server);
+
+        server.listen(3001);
+
+        setInterval(() => {
+            fetch('https://peer-dns.onrender.com')
+                .then((response) => response.text())
+                .then((value) => console.log('Is alive? ', value))
+                .catch((error) => console.log('Error: ', error));
+        }, 4000);
     }
 }
 
